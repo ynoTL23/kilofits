24c494abd633dcc488ecd7c78a4e9a4577ad50d8
ynoTL <tonylu23@gamil.com>
2021-05-31 01:32:54 -0400

    add shop page, CollectionPreview and CollectionItem components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/HomePage'
+import ShopPage from './pages/shop/shop'
 import './App.css'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
