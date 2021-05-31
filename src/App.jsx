dbd41e602146fa0dd4194bee2e8585a28d994ff9
ynoTL <tonylu23@gamil.com>
2021-05-31 02:04:03 -0400

    add header navigation between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/shop'
+import Header from './components/header/header'
+
 import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
