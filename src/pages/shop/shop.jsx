bed41e51eda004041195c4d8650bc88152eefc53
ynoTL23 <tonylu23@gamil.com>
2021-06-02 19:06:01 -0400

    move 'directory' and 'collections' state into redux

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import SHOP_DATA from './shop-data'
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
 
-export default class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+)
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...collectionProps }) => (
-          <CollectionPreview key={id} {...collectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+export default ShopPage
