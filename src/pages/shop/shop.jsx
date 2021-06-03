b6929099240c7db02170c28d8f4a16b0e8552d48
Tony <tonylu23@gmail.com>
2021-06-02 20:10:52 -0400

    local storage, state into redux, and collection routing (#3)
    * add local storage persistence
    
    * move 'directory' and 'collections' state into redux
    
    * create shop routing for specific collections

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import SHOP_DATA from './shop-data'
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import { Route } from 'react-router'
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
+import CollectionPage from '../collection/collection'
 
-export default class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+)
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...collectionProps }) => (
-          <CollectionPreview key={id} {...collectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+export default ShopPage
---
+++ src/redux/directory/directory-reducer.js
@@ -0,0 +1,45 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      url: 'shop/hats'
+    },
+    {
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      id: 2,
+      url: 'shop/jackets'
+    },
+    {
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      id: 3,
+      url: 'shop/sneakers'
+    },
+    {
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      size: 'large',
+      id: 4,
+      url: 'shop/womens'
+    },
+    {
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      url: 'shop/mens'
+    }
+  ]
+}
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default directoryReducer
